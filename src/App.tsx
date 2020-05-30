54b5e1ec0e1e02864a3e456231ef4f1f35e9fc0e
Minh Lam <ltm1701@gmail.com>
2020-05-29 18:23:57 -0600

    Remove unncessary parts

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,30 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom'
-import { Home } from './components/Home/Home'
-import { PorfolioContainer } from './components/Porfolio/PorfolioContainer'
+import { Switch, Route } from 'react-router-dom';
+import { Home } from './components/Home/Home';
+import { PorfolioContainer } from './components/Porfolio/PorfolioContainer';
 import { Nav } from './components/common/Nav/Nav';
 import { Footer } from './components/common/Footer/Footer';
-import  {BlogContainer} from './components/Blog/BlogList/BlogContainer';
+import { BlogContainer } from './components/Blog/BlogList/BlogContainer';
 
 function App() {
+	return (
+		<div className="App">
+			<Nav />
 
-  return (
-    <div className="App">
-      <Nav />
-
-      <BlogContainer/>
-      {/* <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route exact path="/porfolio">
-          <PorfolioContainer />
-        </Route>
-        <Route exact path="/blog">
-          <Blog />
-        </Route>
-
-      </Switch> */}
-      <Footer />
-    </div>
-  );
+			<Switch>
+				<Route exact path="/">
+					<Home />
+				</Route>
+				<Route exact path="/porfolio">
+					<PorfolioContainer />
+				</Route>
+				<Route exact path="/blog">
+					<BlogContainer />
+				</Route>
+			</Switch>
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
