b703dd1728fb285b28067c0325e73f51c3064709
Minh Lam <ltm1701@gmail.com>
2020-05-29 18:27:34 -0600

    change date format

--- src/components/common/blogCard/BlogCard.tsx
+++ src/components/common/blogCard/BlogCard.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './blogCard.css'
 interface HomeBlog {
     title: string,
-    date: string,
+    date: any,
     content: string,
     id: string,
     className:string
@@ -10,13 +10,14 @@ interface HomeBlog {
 
 
 export const BlogCard: React.FC<HomeBlog> = ({ title, date, content, id,className }) => {
+
     return (
             <div className={className}>
                     <a href={`/${id}`}>
                             <div className="blog-desc">
                                     <h2>{title}</h2>
-                                    <p>{content.slice(0,20)+'...'}</p>
-                                    <p className="blog-date">{date}</p>
+                                    <p>{content.slice(0,90)+'...'}</p>
+                                    <p className="blog-date">{date.toDate().toDateString()}</p>
                             </div>
                     </a>
             </div>
