b38c03f36106808796eb823d88a5864a48e383f8
Minh Lam <ltm1701@gmail.com>
2020-05-30 21:05:24 -0600

    Blog Article Page

--- src/components/Blog/BlogArticle/BlogArticle.tsx
+++ src/components/Blog/BlogArticle/BlogArticle.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
+import ReactMarkDown from 'react-markdown';
+import './blogArticle.css';
 
 interface BlogArticleProps {
-
+	markDown: string;
 }
 
-export const BlogArticle: React.FC<BlogArticleProps> = ({}) => {
-        return (<div></div>);
-}
+export const BlogArticle: React.FC<BlogArticleProps> = ({ markDown }) => {
+	return (
+		<div className="article">
+			<ReactMarkDown escapeHtml={true} source={markDown} />
+		</div>
+	);
+};
--- src/components/Blog/BlogArticle/BlogArticleContainer.tsx
+++ src/components/Blog/BlogArticle/BlogArticleContainer.tsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import { BlogArticle } from './BlogArticle'
+import { blogRef } from '../../../firebase/firebase'
 
 interface BlogArticleContainerProps {
-
+        id: 'string'
 }
 
-export const BlogArticleContainer: React.FC<BlogArticleContainerProps> = ({}) => {
-        return (<div></div>);
+
+
+export const BlogArticleContainer: React.FC<BlogArticleContainerProps> = ({id}) => {
+        const [loading, setLoading] = React.useState(false)
+        const [blog, setBlog] = React.useState('')
+        const [error, setError] = React.useState('')
+        
+        const fetchBlog = ()=>{
+                setLoading(true)
+                blogRef.doc(id).get().then(function(doc){
+                        setLoading(false)
+                        if(doc.exists){
+                                console.log(doc.data())
+                        }else{
+                                setError('Hmmm! I have looked all over the place but still cannot find you blog.')
+                        }
+                })
+        }
+
+        React.useEffect(()=>{
+                
+        },[])
+        return (<BlogArticle markDown={''}/>);
 }
