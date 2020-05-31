f28b556cd07bf62d024b3b2b19af33e2217d4714
Minh Lam <ltm1701@gmail.com>
2020-05-30 21:05:42 -0600

    Remove uncessary import

--- src/components/Home/Blog/BlogContainer.tsx
+++ src/components/Home/Blog/BlogContainer.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import { connect } from "react-redux"
 import { loadBlogs } from '../../../redux/action/action'
 import { Blog } from './Blog'
