256170841a0f5f3358458bbbe2e89967b502bbec
Minh Lam <ltm1701@gmail.com>
2020-05-31 19:45:05 -0600

    Login function on click. this function will also update redux state

--- src/components/Login/LoginComponent.tsx
+++ src/components/Login/LoginComponent.tsx
@@ -1,25 +1,62 @@
 import React from 'react';
-import { Login } from './Login';
-import {authRef} from '../../firebase/firebase'
+import Login  from './Login';
+import { withRouter } from 'react-router-dom';
+import { authRef } from '../../firebase/firebase';
+import { connect } from 'react-redux';
 
-interface LoginComponentProps {}
+/*--PropsType-- */
+interface LoginComponentProps {
+	dispatchSignIn: () => void;
+    history: any;
+    isAuth: any
+}
 
-export const LoginComponent: React.FC<LoginComponentProps> = ({}) => {
-	const [ email, setEmail ] = React.useState('');
-    const [ password, setPassword ] = React.useState('');
-    const [error, setError] = React.useState('');
-    
-    const signIn = (ev: any) => {
-        ev.preventDefault();
-        //TODO implment auth with redux
+/*--Dispatch to Props for redux-- */
+function mapDispatchToProps(dispatch: any) {
+	return {
+		dispatchSignIn: () => dispatch({ type: 'LOGIN' })
+	};
+}
+
+function testing(state:any){
+    return{
+        isAuth: state
     }
+}
+
+
+const LoginComponent: React.FC<LoginComponentProps> = ({ dispatchSignIn, history, isAuth }) => {
+	const [ email, setEmail ] = React.useState('');
+	const [ password, setPassword ] = React.useState('');
+	const [ error, setError ] = React.useState('');
+
+    console.log(isAuth)
+    /**
+     * function for signin admin view
+     * @param ev event
+     */
+	const logIn = (ev: any) => {
+		ev.preventDefault();
+
+		authRef
+			.signInWithEmailAndPassword(email, password)
+			.then(() => {
+                dispatchSignIn()
+                history.push('/dashboard');
+			})
+        	.catch((error)=> {setError(error.message);console.log(error)});
+        // authRef.signOut()
+	};
 
 	return (
 		<Login
 			emailOnChange={(ev) => setEmail(ev.target.value)}
-            passwordOnChange={(ev) => setPassword(ev.target.value)}
-            signIn={signIn}
+			passwordOnChange={(ev) => setPassword(ev.target.value)}
+			logIn={logIn}
             error={error}
+            key="login"
 		/>
 	);
 };
+
+export default connect(testing, mapDispatchToProps)(withRouter(LoginComponent));
