f8ca0312e5e90c88d18ee9ec3f58e14c2e90815e
Minh Lam <ltm1701@gmail.com>
2020-06-01 11:00:22 -0600

    add key for porfolio component

--- src/components/Porfolio/Porfolio.tsx
+++ src/components/Porfolio/Porfolio.tsx
@@ -17,7 +17,7 @@ export const Porfolio: React.FC<PorfolioProps> = ({ porfolioList }) => {
                         {
                                 porfolioList.map((porfolio) => {
                                         return (
-                                                <div className="col-3 porfolio-card">
+                                                <div className="col-3 porfolio-card" key={porfolio.url}>
                                                         <a target="_blank" rel="noopener noreferrer" href={porfolio.url}>
                                                                 <div className="porfolio-img" style={{ backgroundImage: `url(${porfolio.img})` }}></div>
                                                                 <div className="porfolio-content">
