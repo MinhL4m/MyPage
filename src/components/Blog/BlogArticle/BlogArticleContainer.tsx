d992c82067e779460c733dee96a4839a52617b41
Minh Lam <ltm1701@gmail.com>
2020-06-01 10:58:54 -0600

    add fetch blog

--- src/components/Blog/BlogArticle/BlogArticleContainer.tsx
+++ src/components/Blog/BlogArticle/BlogArticleContainer.tsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import { BlogArticle } from './BlogArticle'
 import { blogRef } from '../../../firebase/firebase'
+import {useParams} from 'react-router-dom'
 
 interface BlogArticleContainerProps {
-        id: 'string'
+
 }
 
 
 
-export const BlogArticleContainer: React.FC<BlogArticleContainerProps> = ({id}) => {
+export const BlogArticleContainer: React.FC<BlogArticleContainerProps> = ({}) => {
+        const {id} = useParams()
+
         const [loading, setLoading] = React.useState(false)
         const [blog, setBlog] = React.useState('')
         const [error, setError] = React.useState('')
@@ -18,7 +21,9 @@ export const BlogArticleContainer: React.FC<BlogArticleContainerProps> = ({id}) 
                 blogRef.doc(id).get().then(function(doc){
                         setLoading(false)
                         if(doc.exists){
-                                console.log(doc.data())
+                                const obj = doc.data() as any
+                                let content = `# ${obj.title}\n*${obj.date.toDate().toDateString()}*\n\n${obj.content}`
+                                setBlog(content)
                         }else{
                                 setError('Hmmm! I have looked all over the place but still cannot find you blog.')
                         }
@@ -26,7 +31,7 @@ export const BlogArticleContainer: React.FC<BlogArticleContainerProps> = ({id}) 
         }
 
         React.useEffect(()=>{
-                
+                fetchBlog()
         },[])
-        return (<BlogArticle markDown={''}/>);
+        return (<BlogArticle markDown={blog}/>);
 }
