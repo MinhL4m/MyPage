ba6c7ab9b7c5557262055a92c24b87df78ce9947
Minh Lam <ltm1701@gmail.com>
2020-05-31 19:45:36 -0600

    fetch CurrentUser when component first mount

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,41 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Switch, Route } from 'react-router-dom';
 import { Home } from './components/Home/Home';
 import { PorfolioContainer } from './components/Porfolio/PorfolioContainer';
 import { Nav } from './components/common/Nav/Nav';
 import { Footer } from './components/common/Footer/Footer';
 import { BlogContainer } from './components/Blog/BlogList/BlogContainer';
+import { BlogArticle } from './components/Blog/BlogArticle/BlogArticle';
+import Login from './components/Login/LoginComponent';
 
-function App() {
+import { authRef } from './firebase/firebase';
+
+function mapDispatchToProps(dispatch: any) {
+	return {
+		dispatchSignIn: () => dispatch({ type: 'LOGIN' })
+	};
+}
+
+function App(props: any) {
+	React.useEffect(() => {
+		authRef.onAuthStateChanged((user) => {
+			if (user) {
+				props.dispatchSignIn();
+			}
+		});
+	}, []);
+
 	return (
 		<div className="App">
 			<Nav />
-
 			<Switch>
+				<Route path="/">
+					<Login />
+				</Route>
+			</Switch>
+
+			{/* <Switch>
 				<Route exact path="/">
 					<Home />
 				</Route>
@@ -21,10 +45,10 @@ function App() {
 				<Route exact path="/blog">
 					<BlogContainer />
 				</Route>
-			</Switch>
+			</Switch> */}
 			<Footer />
 		</div>
 	);
 }
 
-export default App;
+export default connect(null, mapDispatchToProps)(App);
