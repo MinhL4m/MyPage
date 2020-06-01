2e1b1eb896a541f0f70257d96493109dbf3b2e5e
Minh Lam <ltm1701@gmail.com>
2020-05-31 19:46:23 -0600

    make reducer moresimple

--- src/redux/reducer/authUser.ts
+++ src/redux/reducer/authUser.ts
@@ -5,7 +5,7 @@ const initState = {
 export default (state = initState, action: any) => {
     switch (action.type) {
         case 'LOGIN':
-            return { isAuth: Object.keys(action.payload.user).length > 0 ? true : false }
+            return { isAuth: true  }
         case 'SIGN_OUT':
             return {isAuth: false}
         default:
