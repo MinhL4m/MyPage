7a5466283415b516ad19995735555f6c5d1d87fe
Minh Lam <ltm1701@gmail.com>
2020-06-02 13:11:52 -0600

    implement login and redirect to admin page

--- src/components/Login/LoginComponent.tsx
+++ src/components/Login/LoginComponent.tsx
@@ -1,54 +1,58 @@
 import React from 'react';
-import Login  from './Login';
+import Login from './Login';
 import { withRouter } from 'react-router-dom';
 import { authRef } from '../../firebase/firebase';
 import { connect } from 'react-redux';
 
 /*--PropsType-- */
 interface LoginComponentProps {
-	dispatchSignIn: () => void;
-    history: any;
-    isAuth: any
+	dispatchLogIn: () => void;
+	history: any;
 }
 
 /*--Dispatch to Props for redux-- */
 function mapDispatchToProps(dispatch: any) {
 	return {
-		dispatchSignIn: () => dispatch({ type: 'LOGIN' })
+		dispatchLogIn: () => dispatch({ type: 'LOGIN' })
 	};
 }
 
-
-const LoginComponent: React.FC<LoginComponentProps> = ({ dispatchSignIn, history }) => {
+const LoginComponent: React.FC<LoginComponentProps> = ({ dispatchLogIn, history }) => {
 	const [ email, setEmail ] = React.useState('');
 	const [ password, setPassword ] = React.useState('');
 	const [ error, setError ] = React.useState('');
 
-    /**
+	// React.useEffect(() => {
+	// 	console.log(email)
+	// });
+
+	/**
      * function for signin admin view
      * @param ev event
      */
 	const logIn = (ev: any) => {
 		ev.preventDefault();
-
 		authRef
-			.signInWithEmailAndPassword(email, password)
+			.signInWithEmailAndPassword( email,  password)
 			.then(() => {
-                dispatchSignIn()
-                history.push('/dashboard');
+		        dispatchLogIn()
+		        history.push('/admin');
 			})
-        	.catch((error)=> {setError(error.message);console.log(error)});
-        // authRef.signOut()
+			.catch((error)=> {setError(error.message)});
 	};
 
 	return (
+		<>
 		<Login
 			emailOnChange={(ev) => setEmail(ev.target.value)}
-			passwordOnChange={(ev) => setPassword(ev.target.value)}
+			passwordOnChange={(ev) => {
+				setPassword(ev.target.value);
+			}}
 			logIn={logIn}
-            error={error}
-            key="login"
+			error={error}
+			key="login"
 		/>
+		</>
 	);
 };
 
