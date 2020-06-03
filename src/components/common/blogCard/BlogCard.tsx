d915e4a1c876bfa523ddeecbd5e8e70cc177ec11
Minh Lam <ltm1701@gmail.com>
2020-06-03 11:28:45 -0600

    If this card display on admin blog list then the url will go to /admin/blog

--- src/components/common/blogCard/BlogCard.tsx
+++ src/components/common/blogCard/BlogCard.tsx
@@ -6,15 +6,16 @@ interface HomeBlog {
     date: any,
     content: string,
     id: string,
-    className:string
+    className:string,
+    admin?:boolean
 }
 
 
-export const BlogCard: React.FC<HomeBlog> = ({ title, date, content, id,className }) => {
+export const BlogCard: React.FC<HomeBlog> = ({ title, date, content, id,className, admin = false }) => {
 
     return (
             <div className={className}>
-                    <Link to={`/blog/${id}`}>
+                    <Link to={admin?`/admin/blog/${id}`:`/blog/${id}`}>
                             <div className="blog-desc">
                                     <h2>{title}</h2>
                                     <p>{content.slice(0,90)+'...'}</p>
