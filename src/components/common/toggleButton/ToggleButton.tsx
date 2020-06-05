5cd8e6eb013105f65bfe90468ccb4429900267f9
Minh Lam <ltm1701@gmail.com>
2020-06-04 19:00:10 -0600

    minor change for toggle button

--- src/components/common/toggleButton/ToggleButton.tsx
+++ src/components/common/toggleButton/ToggleButton.tsx
@@ -9,12 +9,11 @@ interface ToggleButtonProps {
 }
 
 export const ToggleButton: React.FC<ToggleButtonProps> = ({ value, on = 'Markup', off="Text", onToggle}) => {
-
 	return (
         <div className="toggle-btn">
             <span>{off}</span>
             <input checked={value} type="checkbox" id="toggle_checkbox" onClick={onToggle}/>
-            <label htmlFor="toggle_checkbox"></label>
+            <label className="toogle-label" htmlFor="toggle_checkbox"></label>
             <span>{on}</span>
         </div>
 	);
