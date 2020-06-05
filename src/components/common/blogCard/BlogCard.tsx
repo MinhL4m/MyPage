3ec3c2ba04a31227d3dc4ad6f85aff5fece440aa
Minh Lam <ltm1701@gmail.com>
2020-06-05 15:46:29 -0600

    remove content

--- src/components/common/blogCard/BlogCard.tsx
+++ src/components/common/blogCard/BlogCard.tsx
@@ -4,21 +4,19 @@ import './blogCard.css'
 interface HomeBlog {
     title: string,
     date: any,
-    content: string,
     id: string,
     className:string,
     admin?:boolean
 }
 
 
-export const BlogCard: React.FC<HomeBlog> = ({ title, date, content, id,className, admin = false }) => {
+export const BlogCard: React.FC<HomeBlog> = ({ title, date, id,className, admin = false }) => {
 
     return (
             <div className={className}>
                     <Link to={admin?`/admin/blog/${id}`:`/blog/${id}`}>
                             <div className="blog-desc">
                                     <h2>{title}</h2>
-                                    <p>{content.slice(0,90)+'...'}</p>
                                     <p className="blog-date">{date.toDate().toDateString()}</p>
                             </div>
                     </Link>
