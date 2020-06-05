2b6a7a57d07d08a9e9ce477cb2ae2260f12cc247
Minh Lam <ltm1701@gmail.com>
2020-06-05 15:46:17 -0600

    change url of the icons

--- src/components/common/Icon/Icons.tsx
+++ src/components/common/Icon/Icons.tsx
@@ -10,9 +10,9 @@ interface IconsProps {
 export const Icons: React.FC<IconsProps> = ({ className }) => {
     return (
         <ul className={className}>
-            <li><a href="https://www.google.com"><FontAwesomeIcon icon={faLinkedin} /></a></li>
-            <li><a href="https://www.google.com"><FontAwesomeIcon icon={faFacebookSquare} /></a></li>
-            <li><a href="https://www.google.com"><FontAwesomeIcon icon={faGithubSquare} /></a></li>
+            <li><a href="https://www.linkedin.com/in/minh-lam-1701/"><FontAwesomeIcon icon={faLinkedin} /></a></li>
+            <li><a href="https://www.facebook.com/minhlam1701/"><FontAwesomeIcon icon={faFacebookSquare} /></a></li>
+            <li><a href="https://github.com/MinhL4m"><FontAwesomeIcon icon={faGithubSquare} /></a></li>
         </ul>
     );
 }
