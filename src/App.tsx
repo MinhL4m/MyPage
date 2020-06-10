4a173df0deb694cc080cddc5eb67781abe7fabe9
Minh Lam <ltm1701@gmail.com>
2020-06-09 19:13:47 -0600

    add new route

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,19 @@ import { PorfolioContainer } from './components/Porfolio/PorfolioContainer';
 import Nav from './components/common/Nav/Nav';
 import { Footer } from './components/common/Footer/Footer';
 import { BlogContainer } from './components/Blog/BlogList/BlogContainer';
-import {BlogContainer as AdminBlog} from './components/Admin/BlogList/BlogContainer'
+import { BlogContainer as AdminBlog } from './components/Admin/BlogList/BlogContainer'
 import { BlogArticleContainer } from './components/Blog/BlogArticle/BlogArticleContainer';
 import Login from './components/Login/LoginComponent';
-import {Admin} from './components/Admin/Admin/Admin'
+import { Admin } from './components/Admin/Admin/Admin'
 
 import { authRef } from './firebase/firebase';
 import NotLoginRoute from './components/common/PrivateRoute/NotLoginRoute';
 import LoginedRoute from './components/common/PrivateRoute/LoginedRoute';
 
-import BlogNewContainer  from './components/Admin/BlogEdit/BlogNewContainer';
-import BlogEditContainer  from './components/Admin/BlogEdit/BlogEditContainer';
+import BlogNewContainer from './components/Admin/BlogEdit/BlogNewContainer';
+import BlogEditContainer from './components/Admin/BlogEdit/BlogEditContainer';
+import { TodoContainer } from './components/Admin/Todo/TodoContainer';
+
 
 
 function mapDispatchToProps(dispatch: any) {
@@ -26,21 +28,18 @@ function mapDispatchToProps(dispatch: any) {
 }
 
 function App(props: any) {
-	React.useEffect(() => {
-		authRef.onAuthStateChanged((user) => {
-			if (user) {
-				props.dispatchSignIn();
-			}
-			document.querySelector('.App')?.classList.add('render')
-		});
-	}, []);
+	// React.useEffect(() => {
+	// 	authRef.onAuthStateChanged((user) => {
+	// 		if (user) {
+	// 			props.dispatchSignIn();
+	// 		}
+	// 		document.querySelector('.App')?.classList.add('render')
+	// 	});
+	// }, []);
 
 	return (
-		<div className="App">
+		<div className="App render">
 			<Nav />
-			<Switch>
-				
-			</Switch>
 			<Switch>
 				<Route onUpdate={() => window.scrollTo(0, 0)} exact path="/">
 					<Home />
@@ -51,14 +50,15 @@ function App(props: any) {
 				<Route onUpdate={() => window.scrollTo(0, 0)} exact path="/blog">
 					<BlogContainer />
 				</Route>
-				<Route onUpdate={() => window.scrollTo(0, 0)} exact path ="/blog/:id" children={<BlogArticleContainer/>}></Route>
-				
-				<NotLoginRoute Component={Login} path="/login"/>
-				<LoginedRoute Component={BlogNewContainer} path="/admin/addblog"/>
-				<LoginedRoute Component={BlogEditContainer} path="/admin/blog/:id"/>
-				<LoginedRoute Component={AdminBlog} path="/admin/blog"/>
-				<LoginedRoute Component={Admin} path="/admin"/>
-				
+				<Route onUpdate={() => window.scrollTo(0, 0)} exact path="/blog/:id" children={<BlogArticleContainer />}></Route>
+
+				<NotLoginRoute Component={Login} path="/login" />
+				<LoginedRoute Component={BlogNewContainer} path="/admin/addblog" />
+				<LoginedRoute Component={BlogEditContainer} path="/admin/blog/:id" />
+				<LoginedRoute Component={TodoContainer} path="/admin/todo" />
+				<LoginedRoute Component={AdminBlog} path="/admin/blog" />
+				<LoginedRoute Component={Admin} path="/admin" />
+
 			</Switch>
 			<Footer />
 		</div>
