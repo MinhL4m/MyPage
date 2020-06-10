002f64d72a7daaa02fcb24b391c15687bd67f5e5
Minh Lam <ltm1701@gmail.com>
2020-06-09 19:14:58 -0600

    add id for loadTodo

--- src/redux/action/action.ts
+++ src/redux/action/action.ts
@@ -18,9 +18,8 @@ export function loadTodo() {
 		todoRef.get().then((snapshot) => {
 			let docs = [] as Array<any>;
 			snapshot.forEach((doc) => {
-				docs.push(doc.data());
+				docs.push({id:doc.id,...doc.data()});
 			});
-
 			dispatch({ type: 'LOAD_TODO', payload: { todos: docs } });
 		});
 	};
